28bbb520bc3633226067ff691bdc787596df4cd4
Mukhtar <mukhtar@example.com>
2026-08-19 16:17:57 +0300

    Submited Exercise 2

---
+++ Exercise 2/exercise 2.js
@@ -0,0 +1,7 @@
+let names = ["Alice", "Bob", "Charlie"];
+let grades = [90, 85, 78];
+let status = [true, false, true];
+let products = ["Phone", "Laptop"];
+let location = ["New York", 40.7128, -74.006];
+console.log(location);
+export {};
---
+++ Exercise 2/exercise 2.ts
@@ -0,0 +1,10 @@
+export {};
+
+let names: string[] = ["Alice", "Bob", "Charlie"];
+let grades: number[] = [90, 85, 78];
+let status: boolean[] = [true, false, true];
+
+let products: string[] = ["Phone", "Laptop"];
+
+let location: [string, number, number] = ["New York", 40.7128, -74.006];
+console.log(location);
